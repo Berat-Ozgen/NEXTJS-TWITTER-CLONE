f830cf14a314f52242d01ce2f4c9f926f1250acb
Berat Ozgen <berat.ozgnn.01@gmail.com>
2023-06-14 20:05:20 +0300

    Layout
    Layout

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,47 @@
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+import { BiLogOut } from "react-icons/bi";
+import SidebarLogo from "./SidebarLogo";
+import SidebarItem from "./SidebarItem";
+import SidebarTweetButton from "./SidebarTweetButton";
+
+const Sidebar = () => {
+  const items = [
+    {
+      icon: BsHouseFill,
+      label: "Home",
+      href: "/",
+    },
+    {
+      icon: BsBellFill,
+      label: "Notifications",
+      href: "/notifications",
+    },
+    {
+      icon: FaUser,
+      label: "Profile",
+      href: "/users/123",
+    },
+  ];
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end text-red-400">
+        <div className="space-y-2 lg:w-[230px] ">
+          <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+            />
+          ))}
+          <SidebarItem onClick={() => {}} icon={BiLogOut} label="logout" />
+          <SidebarTweetButton />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
